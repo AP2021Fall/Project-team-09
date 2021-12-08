16c69c34a76429ed8169e4a7226713f6a0a71514
donya <donyaroshanzamir81@gmail.com>
2021-12-08 21:33:33 +0330

    update task model

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,57 @@
 package model;
 
+import java.util.ArrayList;
+
+
 public class User {
+
+    private static ArrayList<User> allUsers = new ArrayList<>();
+    private String lastName;
+    private String birthday;
+    private String username;
+    private String password1;
+    private String password2;
+    private String email;
+    private ArrayList<Task> taskOfUser;
+
+    private class teamMember {
+
+    }
+    private class teamLeader{
+
+    }
+    private class systemAdministrator{
+
+    }
+
+    public User(String username,String password1,String password2, String email ) {
+        this.username = username;
+        this.password1 = password1;
+        this.password2 = password2;
+        this.email = email;
+        this.taskOfUser = new ArrayList<>();
+        allUsers.add(this);
+    }
+
+    public static ArrayList<User> getAllUsers() {
+        return allUsers;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
     public static boolean usernameExists(String username) {
         return false;
